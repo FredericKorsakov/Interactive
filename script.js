754b79cb82571eff9b7edb67de3abbc5d028ca17
FredericKorsakov <100227451+FredericKorsakov@users.noreply.github.com>
2022-12-22 10:19:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,26 @@
 
 
-const btns = document.querySelectorAll('[role="button"]');
-const submitBtn = document.getElementById('submit');
-const feedBackBox = document.getElementById('feedback-box');
-const thankYouBox = document.getElementById('thank-you-box');
-const selectedRating = document.getElementById('selected-rating');
 
 
-btns.forEach((btn) => {
+const mainContainer = document.querySelector(".container")
+const thanksContainer = document.querySelector(".thank-you")
+const submitButton = document.getElementById("submit-rating")
+const rateAgain = document.getElementById("rate-again")
+const ratings = document.querySelectorAll(".btn")
+const actualRating = document.getElementById("rating")
 
-    btn.addEventListener('click', function (e) {
+submitButton.addEventListener("click", () => {
+    mainContainer.style.display = "none"
+    thanksContainer.classList.remove("hidden")
 
-        let selectedBtn = e.currentTarget;
-        for (let i = 0; i < btns.length; i++) {
-
-            if (btns[i].getAttribute('aria-selected') == 'true') {
-
-                btns[i].setAttribute('aria-selected', false);
-                selectedBtn.setAttribute('aria-selected', true);
-                selectedRating.textContent = selectedBtn.id;
-
-
-            }
-            else {
-                selectedBtn.setAttribute('aria-selected', true);
-                selectedRating.textContent = selectedBtn.id;
-            }
-
-        }
-
+    ratings.forEach((rating) => {
+        rating.addEventListener("click", () => {
+            actualRating.innerHTML = rating.innerHTML
+        })
     })
-
-
 })
 
-submitBtn.addEventListener('click', function () {
-
-    feedBackBox.setAttribute('hidden', true);
-    thankYouBox.removeAttribute('hidden');
-
-
-
+rateAgain.addEventListener("click", () => {
+    mainContainer.style.display = "block"
+    thanksContainer.classList.add("hidden")
 })
